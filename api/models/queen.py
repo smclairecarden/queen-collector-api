5b0e78d4c601444b866f31e51e539de98da36e15
Claire Carden <smclairecarden@gmail.com>
2022-05-06 20:32:14 -0500

    create queen table

---
+++ api/models/queen.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from api.models.db import db
+
+class Queen(db.Model):
+    __tablename__ = 'queens'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    season = db.Column(db.Integer)
+    description = db.Column(db.String(250))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+
+    def __repr__(self):
+      return f"Queen('{self.id}', '{self.name}'"
+
+    def serialize(self):
+      queen = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+      return queen
